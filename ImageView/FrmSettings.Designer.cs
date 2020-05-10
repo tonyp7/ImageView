fd744a75c82cdf5e11ad2ec4e8ecc89d9c1e3409
tonyp7 <bubbathemaster@gmail.com>
2020-05-10 13:00:24 +0800

    layout of the form revised

--- ImageView/FrmSettings.Designer.cs
+++ ImageView/FrmSettings.Designer.cs
@@ -29,10 +29,284 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPageGeneral = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHistoryMaxSize = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chkHistorySaveOnExit = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSlideshowTimer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnMakeDefault = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabControl.SuspendLayout();
+            this.tabPageGeneral.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPageGeneral);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(624, 378);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabPageGeneral
+            // 
+            this.tabPageGeneral.Controls.Add(this.groupBox3);
+            this.tabPageGeneral.Controls.Add(this.groupBox2);
+            this.tabPageGeneral.Controls.Add(this.groupBox1);
+            this.tabPageGeneral.Location = new System.Drawing.Point(4, 22);
+            this.tabPageGeneral.Name = "tabPageGeneral";
+            this.tabPageGeneral.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageGeneral.Size = new System.Drawing.Size(616, 352);
+            this.tabPageGeneral.TabIndex = 0;
+            this.tabPageGeneral.Text = "General";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.txtHistoryMaxSize);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.chkHistorySaveOnExit);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(3, 225);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(610, 124);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "History";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "A value of 0 will disable history. The maximum value is 99.";
+            // 
+            // txtHistoryMaxSize
+            // 
+            this.txtHistoryMaxSize.Location = new System.Drawing.Point(181, 54);
+            this.txtHistoryMaxSize.MaxLength = 3;
+            this.txtHistoryMaxSize.Name = "txtHistoryMaxSize";
+            this.txtHistoryMaxSize.Size = new System.Drawing.Size(45, 20);
+            this.txtHistoryMaxSize.TabIndex = 5;
+            this.toolTip.SetToolTip(this.txtHistoryMaxSize, "Values are in milliseconds. The default of 5000 means 5 seconds.");
+            this.txtHistoryMaxSize.TextChanged += new System.EventHandler(this.txtHistoryMaxSize_TextChanged);
+            this.txtHistoryMaxSize.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumeric_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Number of items to keep in history:";
+            // 
+            // chkHistorySaveOnExit
+            // 
+            this.chkHistorySaveOnExit.AutoSize = true;
+            this.chkHistorySaveOnExit.Location = new System.Drawing.Point(6, 28);
+            this.chkHistorySaveOnExit.Name = "chkHistorySaveOnExit";
+            this.chkHistorySaveOnExit.Size = new System.Drawing.Size(194, 17);
+            this.chkHistorySaveOnExit.TabIndex = 1;
+            this.chkHistorySaveOnExit.Text = "Save History When Exiting Program";
+            this.chkHistorySaveOnExit.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtSlideshowTimer);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(3, 152);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(610, 73);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Slideshow";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(199, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ms";
+            // 
+            // txtSlideshowTimer
+            // 
+            this.txtSlideshowTimer.Location = new System.Drawing.Point(147, 24);
+            this.txtSlideshowTimer.MaxLength = 7;
+            this.txtSlideshowTimer.Name = "txtSlideshowTimer";
+            this.txtSlideshowTimer.Size = new System.Drawing.Size(45, 20);
+            this.txtSlideshowTimer.TabIndex = 3;
+            this.toolTip.SetToolTip(this.txtSlideshowTimer, "Values are in milliseconds. The default of 5000 means 5 seconds.");
+            this.txtSlideshowTimer.TextChanged += new System.EventHandler(this.txtSlideshowTimer_TextChanged);
+            this.txtSlideshowTimer.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumeric_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Time to display each image:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnMakeDefault);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(610, 149);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Default Application";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(6, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(598, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Click on the button below to launch Windows Default Applications. From there, you" +
+    " can select ImageView to be your default Photo Viewer.";
+            // 
+            // btnMakeDefault
+            // 
+            this.btnMakeDefault.Location = new System.Drawing.Point(202, 77);
+            this.btnMakeDefault.Name = "btnMakeDefault";
+            this.btnMakeDefault.Size = new System.Drawing.Size(200, 40);
+            this.btnMakeDefault.TabIndex = 1;
+            this.btnMakeDefault.Text = "Make Default Viewer";
+            this.btnMakeDefault.UseVisualStyleBackColor = true;
+            this.btnMakeDefault.Click += new System.EventHandler(this.btnMakeDefault_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnApply);
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btnOK);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 378);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(624, 63);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnCancel.Location = new System.Drawing.Point(5, 5);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(120, 53);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnApply
+            // 
+            this.btnApply.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnApply.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnApply.Location = new System.Drawing.Point(369, 5);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(120, 53);
+            this.btnApply.TabIndex = 1;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnOK.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnOK.Location = new System.Drawing.Point(499, 5);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(120, 53);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(489, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 53);
+            this.panel2.TabIndex = 3;
+            // 
+            // FrmSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FrmSettings";
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmSettings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Settings";
+            this.Load += new System.EventHandler(this.FrmSettings_Load);
+            this.tabControl.ResumeLayout(false);
+            this.tabPageGeneral.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPageGeneral;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnMakeDefault;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSlideshowTimer;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtHistoryMaxSize;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox chkHistorySaveOnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Panel panel2;
     }
 }
--- ImageView/FrmSettings.cs
+++ ImageView/FrmSettings.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,84 @@ namespace ImageView
 {
     public partial class FrmSettings : Form
     {
-        public FrmSettings()
+        private Config configOriginal;
+        private Config configNew;
+
+
+
+        private void loadUIElements()
+        {
+            txtSlideshowTimer.Text = configNew.Slideshow.Timer.ToString();
+            chkHistorySaveOnExit.Checked = configNew.History.SaveOnExit;
+            txtHistoryMaxSize.Text = configNew.History.Size.ToString();
+        }
+
+        public FrmSettings(Config config)
         {
             InitializeComponent();
+            this.configOriginal = config;
+            this.configNew = (Config)config.Clone();
+            loadUIElements();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmSettings_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMakeDefault_Click(object sender, EventArgs e)
+        {
+            Program.LaunchURL("ms-settings:defaultapps");
+            //bool result = await System.Launcher.LaunchUriAsync(new Uri("ms-settings:defaultapps"));
+
+        }
+
+
+
+        private void validateNumericTextBox(object sender, EventArgs e, int defaultValue, int maxValue)
+        {
+            TextBox t = (TextBox)sender;
+            t.Text = Regex.Replace(t.Text, "[^0-9]", "");
+
+            uint i = 0;
+            if (uint.TryParse(t.Text, out i))
+            {
+                if (i > maxValue)
+                {
+                    t.Text = maxValue.ToString();
+                }
+            }
+            else
+            {
+                t.Text = defaultValue.ToString();
+            }
+        }
+
+
+        private void txtNumeric_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void txtHistoryMaxSize_TextChanged(object sender, EventArgs e)
+        {
+            validateNumericTextBox(sender, e, ConfigHistory.DEFAULT_HISTORY_SIZE, ConfigHistory.MAXIMUM_HISTORY_SIZE);
+        }
+
+        private void txtSlideshowTimer_TextChanged(object sender, EventArgs e)
+        {
+            validateNumericTextBox(sender, e, ConfigSlideshow.DEFAULT_SLIDESHOW_TIMER, ConfigSlideshow.MAXIMUM_SLIDESHOW_TIMER);
         }
     }
 }
