42ef3ddbf066e3057977cbe4446ab7270854c2d9
tonyp7 <bubbathemaster@gmail.com>
2020-05-21 20:01:38 +0800

    Custom PictureBox. A bit of a place holder for now

---
+++ ImageView/PictureBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageView
+{
+    public class PictureBox : System.Windows.Forms.PictureBox
+    {
+        public InterpolationMode InterpolationMode { get; set; }
+
+        protected override void OnPaint(PaintEventArgs paintEventArgs)
+        {
+            paintEventArgs.Graphics.InterpolationMode = InterpolationMode;
+            base.OnPaint(paintEventArgs);
+        }
+
+
+    }
+}
