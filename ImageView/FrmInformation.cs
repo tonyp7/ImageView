a67ec1cb378206a4f4c8fc6dd7585f842e2ca200
tonyp7 <bubbathemaster@gmail.com>
2020-05-23 22:48:43 +0800

    Update info to use ImageMagick instead of native PropertyItems

--- ImageView/FrmInformation.cs
+++ ImageView/FrmInformation.cs
@@ -23,10 +23,12 @@ SOFTWARE.
 
  */
 
+using ImageMagick;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
@@ -47,68 +49,77 @@ namespace ImageView
 
         private void FrmInformation_Load(object sender, EventArgs e)
         {
+
+
             //file
             dgvFile.Rows.Add("Name", workingData.fileInfo.Name);
-            dgvFile.Rows.Add("Dimensions", String.Format("{0} x {1}", workingData.image.Width, workingData.image.Height));
-            dgvFile.Rows.Add("Pixel Format", Program.HumanReadablePixelFormat(workingData.image.PixelFormat));
+
+            //image relation
+            dgvFile.Rows.Add("Dimensions", String.Format("{0} x {1}", workingData.nativeImage.BaseWidth, workingData.nativeImage.BaseHeight));
+            dgvFile.Rows.Add("Format", workingData.nativeImage.Format.ToString());
+            dgvFile.Rows.Add("Color Space", workingData.nativeImage.ColorSpace.ToString()  );
+            dgvFile.Rows.Add("Color Type", workingData.nativeImage.ColorType.ToString() );
+            //dgvFile.Rows.Add("Unique colors", workingData.nativeImage.TotalColors.ToString() );  //very slow
+
+
+            //file related
             dgvFile.Rows.Add("Size (bytes)", workingData.fileInfo.Length.ToString());
             dgvFile.Rows.Add("Created", workingData.fileInfo.CreationTime.ToString());
             dgvFile.Rows.Add("Last Written", workingData.fileInfo.LastWriteTime.ToString());
             dgvFile.Rows.Add("Path", workingData.fileInfo.DirectoryName);
             dgvFile.Rows.Add("File Attributes", workingData.fileInfo.Attributes.ToString());
 
-
-            //exif
-            foreach (PropertyItem property in workingData.propertyItems)
+            
+            IExifProfile profile = workingData.nativeImage.GetExifProfile();
+            if (profile != null)
             {
-                object propValue;
-                switch ((PropertyTagType)property.Type)
+                foreach (IExifValue value in profile.Values)
                 {
-                    case PropertyTagType.ASCII:
-                        ASCIIEncoding encoding = new ASCIIEncoding();
-                        propValue = encoding.GetString(property.Value, 0, property.Len - 1);
-                        break;
-                    case PropertyTagType.Int16:
-                        propValue = BitConverter.ToUInt16(property.Value, 0);
-                        break;
-                    case PropertyTagType.SLONG:
-                        propValue = BitConverter.ToInt32(property.Value, 0);
-                        break;
-                    case PropertyTagType.Int32:
-                        propValue = BitConverter.ToUInt32(property.Value, 0);
-                        break;
-                    case PropertyTagType.SRational:
-                        int sdividend = BitConverter.ToInt32(property.Value, 0);
-                        int sdivisor = BitConverter.ToInt32(property.Value, 4);
-                        if (sdivisor == 0)
-                        {
-                            propValue = double.NaN;
-                        }
-                        else
-                        {
-                            propValue = Math.Round(((double)sdividend / (double)sdivisor), 1);
-                        }
-                        break;
-                    case PropertyTagType.Rational:
-                        uint dividend = BitConverter.ToUInt32(property.Value, 0);
-                        uint divisor = BitConverter.ToUInt32(property.Value, 4);
-                        if(divisor == 0)
-                        {
-                            propValue = double.NaN;
-                        }
-                        else
-                        {
-                            propValue = Math.Round( ((double)dividend / (double)divisor), 1);
-                        }
-                        break;
-                    case PropertyTagType.Undefined:
-                    default:
-                        propValue = String.Empty;
-                        break;
+                    dgvExif.Rows.Add(value.Tag.ToString(), value.ToString());
                 }
-
-                dgvExif.Rows.Add((PropertyTagId)property.Id, propValue.ToString());
             }
+
+
+            //            propValue = BitConverter.ToUInt16(property.Value, 0);
+            //            break;
+            //        case PropertyTagType.SLONG:
+            //            propValue = BitConverter.ToInt32(property.Value, 0);
+            //            break;
+            //        case PropertyTagType.Int32:
+            //            propValue = BitConverter.ToUInt32(property.Value, 0);
+            //            break;
+            //        case PropertyTagType.SRational:
+            //            int sdividend = BitConverter.ToInt32(property.Value, 0);
+            //            int sdivisor = BitConverter.ToInt32(property.Value, 4);
+            //            if (sdivisor == 0)
+            //            {
+            //                propValue = double.NaN;
+            //            }
+            //            else
+            //            {
+            //                propValue = Math.Round(((double)sdividend / (double)sdivisor), 1);
+            //            }
+            //            break;
+            //        case PropertyTagType.Rational:
+            //            uint dividend = BitConverter.ToUInt32(property.Value, 0);
+            //            uint divisor = BitConverter.ToUInt32(property.Value, 4);
+            //            if(divisor == 0)
+            //            {
+            //                propValue = double.NaN;
+            //            }
+            //            else
+            //            {
+            //                propValue = Math.Round( ((double)dividend / (double)divisor), 1);
+            //            }
+            //            break;
+            //        case PropertyTagType.Undefined:
+            //        default:
+            //            propValue = String.Empty;
+            //            break;
+            //    }
+
+            //    dgvExif.Rows.Add((PropertyTagId)property.Id, propValue.ToString());
+            //}
 
 
         }
