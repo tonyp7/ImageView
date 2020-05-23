a990e77634c07ce0684ed4bf162c171d00574948
tonyp7 <bubbathemaster@gmail.com>
2020-05-23 22:47:59 +0800

    new form to display license info

---
+++ ImageView/FrmLicense.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ImageView
+{
+    partial class FrmLicense
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtAdditional = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLicense = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnOK, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.txtAdditional, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtLicense, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(661, 505);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(120, 53);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // txtAdditional
+            // 
+            this.txtAdditional.BackColor = System.Drawing.SystemColors.Window;
+            this.txtAdditional.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtAdditional.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdditional.Location = new System.Drawing.Point(3, 267);
+            this.txtAdditional.Multiline = true;
+            this.txtAdditional.Name = "txtAdditional";
+            this.txtAdditional.ReadOnly = true;
+            this.txtAdditional.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAdditional.Size = new System.Drawing.Size(778, 232);
+            this.txtAdditional.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(462, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "In addition, this software depends on additional libraries and art files; under t" +
+    "he following licenses:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(253, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "This software is licensed under the following license:";
+            // 
+            // txtLicense
+            // 
+            this.txtLicense.BackColor = System.Drawing.SystemColors.Window;
+            this.txtLicense.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLicense.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLicense.Location = new System.Drawing.Point(3, 16);
+            this.txtLicense.Multiline = true;
+            this.txtLicense.Name = "txtLicense";
+            this.txtLicense.ReadOnly = true;
+            this.txtLicense.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLicense.Size = new System.Drawing.Size(778, 232);
+            this.txtLicense.TabIndex = 2;
+            // 
+            // FrmLicense
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimizeBox = false;
+            this.Name = "FrmLicense";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "FrmLicense";
+            this.Load += new System.EventHandler(this.FrmLicense_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox txtAdditional;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLicense;
+    }
+}
---
+++ ImageView/FrmLicense.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageView
+{
+    public partial class FrmLicense : Form
+    {
+        public FrmLicense()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLicense_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string exeLocation = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                string license = File.ReadAllText(Path.Combine(exeLocation, "LICENSE"));
+                string additional = File.ReadAllText(Path.Combine(exeLocation, "DEPENDENCIES.md"));
+
+                txtLicense.Text = license;
+                txtAdditional.Text = additional;
+
+                this.ActiveControl = btnOK;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There was an unexpected error while loading the license files.\nPlease refer to https://getimageview.net", "Error loading license files", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
