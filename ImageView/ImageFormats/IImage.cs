c252072bf31d73ff2e8493981a860fd9a2f61b50
tonyp7 <bubbathemaster@gmail.com>
2020-05-25 17:10:18 +0800

    Create IImage.cs

---
+++ ImageView/ImageFormats/IImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageView.ImageFormats
+{
+    /// <summary>
+    /// Place holder for a generic image type
+    /// </summary>
+    public interface IImage : IDisposable
+    {
+        int BaseWidth { get; }
+        int BaseHeight { get; }
+
+        Bitmap ToBitmap();
+
+    }
+}
