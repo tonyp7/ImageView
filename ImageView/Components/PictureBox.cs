73712ac7e89a6b1968dc2fc3ab70ff16c83c500c
tonyp7 <bubbathemaster@gmail.com>
2020-05-25 17:08:37 +0800

    place holder for a picturebox replacement

---
+++ ImageView/Components/PictureBox.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace ImageView.Components
+{
+    /// <summary>
+    /// Place holder for a high performing blitter
+    /// </summary>
+    public partial class PictureBox : UserControl
+    {
+
+        public PictureBox()
+        {
+            InitializeComponent();
+        }
+
+        public Bitmap Image { get; set; }
+        public InterpolationMode InterpolationMode { get; set; }
+
+        protected override void OnPaint(PaintEventArgs paintEventArgs)
+        {
+            
+            if (Image != null)
+            {
+                
+                Rectangle rect = new Rectangle(0, 0, Image.Width, Image.Height);
+                this.Width = rect.Width;
+                this.Height = rect.Height;
+                paintEventArgs.Graphics.DrawImageUnscaledAndClipped(Image, rect);
+            }
+
+        }
+    }
+}
