92f9e4d0ae9b6595bca43a7d5efe406bd48dce4f
tonyp7 <bubbathemaster@gmail.com>
2020-05-31 15:09:19 +0800

    checkered pattern resource

--- ImageView/Properties/Resources.Designer.cs
+++ ImageView/Properties/Resources.Designer.cs
@@ -472,6 +472,16 @@ namespace ImageView.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap transparent16 {
+            get {
+                object obj = ResourceManager.GetObject("transparent16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap verticalflip16 {
             get {
                 object obj = ResourceManager.GetObject("verticalflip16", resourceCulture);
