c06281529bfa6fefdcbfe994389f914186d40150
tonyp7 <bubbathemaster@gmail.com>
2020-06-06 10:04:53 +0800

    16px printer icon

--- ImageView/Properties/Resources.Designer.cs
+++ ImageView/Properties/Resources.Designer.cs
@@ -393,6 +393,16 @@ namespace ImageView.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap printer16 {
+            get {
+                object obj = ResourceManager.GetObject("printer16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap printer32 {
             get {
                 object obj = ResourceManager.GetObject("printer32", resourceCulture);
