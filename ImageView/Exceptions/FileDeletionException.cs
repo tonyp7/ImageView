e038005212e8a5518b465e6dd69f99418dfb2242
tonyp7 <bubbathemaster@gmail.com>
2020-06-12 17:56:08 +0800

    custom exceptions

---
+++ ImageView/Exceptions/FileDeletionException.cs
@@ -0,0 +1,34 @@
+﻿using ImageView.ImageEntry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageView.Exceptions
+{
+    public class FileDeletionException : Exception
+    {
+
+        public new Exception InnerException { get; set; }
+
+        public IEntry Entry { get; set; }
+
+        public new string Message { get; set; }
+
+        public FileDeletionException(Exception innerException, IEntry entry)
+        {
+            this.InnerException = innerException;
+            this.Entry = entry;
+        }
+
+
+        public FileDeletionException(IEntry entry)
+        {
+            this.Entry = entry;
+            this.InnerException = null;
+        }
+
+
+    }
+}
